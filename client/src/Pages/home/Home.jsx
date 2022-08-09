a37e94836f55fa3cea705cf06b1f531fcf351fd3
Ayan <ayan123chowdhury@gmail.com>
2022-08-10 00:48:34 +0530

    new Components added

--- client/src/Pages/home/Home.jsx
+++ client/src/Pages/home/Home.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Featured from '../../Components/Featured';
+import FeaturedProperties from '../../Components/FeaturedProperties';
+import Footer from '../../Components/Footer';
 import Header from '../../Components/Header';
+import MailList from '../../Components/MailList';
 import Navbar from '../../Components/Navbar';
 import PropertyList from '../../Components/PropertyList';
 import './Home.css';
@@ -15,6 +18,9 @@ const Home = () => {
         <h1 className="homeTitle"> Browse by property type </h1>
         <PropertyList />
         <h1 className="homeTitle"> Home guests love</h1>
+        <FeaturedProperties />
+        <MailList />
+        <Footer />
       </div>
     </div>
   );
