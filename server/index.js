f27dbb0ea2f9e8f6b3825c8c89789c772e21a942
Ayan <ayan123chowdhury@gmail.com>
2022-08-17 00:20:39 +0530

    Connected to DB

--- server/index.js
+++ server/index.js
@@ -1,6 +1,32 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
 const app = express();
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    throw err;
+  }
+};
+
+mongoose.connection.on('disconnected', () => {
+  console.log('MongoDB disconnected');
+});
+
+mongoose.connection.on('connected', () => {
+  console.log('MongoDB connected');
+});
+
+app.get('/', (req, res) => {
+  res.send('hello');
+});
 
 app.listen(8000, () => {
+  connect();
   console.log('Connected to backend');
 });
