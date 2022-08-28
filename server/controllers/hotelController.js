9ac53080155ceb5c0a7b9288e22eef435d3944b5
Ayan <ayan123chowdhury@gmail.com>
2022-08-28 22:02:50 +0530

    Connected to backend & other minor changes

--- server/controllers/hotelController.js
+++ server/controllers/hotelController.js
@@ -1,3 +1,4 @@
+import Hotel from '../models/Hotel.js';
 import Hotels from '../models/Hotel.js';
 
 export const createHotel = async (req, res, next) => {
@@ -45,6 +46,21 @@ export const getHotels = async (req, res, next) => {
   try {
     const hotels = await Hotels.find();
     res.status(200).json(hotels);
+  } catch (err) {
+    // res.status(500).json(err);
+    next(err);
+  }
+};
+
+export const countByCity = async (req, res, next) => {
+  const cities = req.query.cities.split(',');
+  try {
+    const list = await Promise.all(
+      cities.map((city) => {
+        return Hotel.countDocuments({ city: city });
+      })
+    );
+    res.status(200).json(list);
   } catch (err) {
     // res.status(500).json(err);
     next(err);
